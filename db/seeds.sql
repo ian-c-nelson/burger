1f1bb583800ea48e94cf4eb0557b3c4eea18254e
LegionXIII <42222236+ian-c-nelson@users.noreply.github.com>
2018-11-13 22:09:07 -0700

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers ( burger_name ) 
+VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers ( burger_name ) 
+VALUES ('Guacamole Burger');
+INSERT INTO burgers ( burger_name ) 
+VALUES ('Banzai Burger');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,57 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".btn-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var devoured = $(this).data("devoured");
+
+    var newDevouredState = {
+      devoured: !devoured
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevouredState);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $("#add-burger-button").on("click", function(event) {
+    var newBurger = {
+      burger_name: $("#burger-name-input").val().trim(),
+      devoured: false
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
